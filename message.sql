77a614a8513a73685247bb111317f1b1e8e7eb8b
raymond-chii <raymondchi56@gmail.com>
2024-06-27 00:03:00 -0400

    update for chat

--- message.sql
+++ message.sql
@@ -4,6 +4,12 @@ CREATE TABLE message (
     receiver_id BIGINT NOT NULL,
     content VARCHAR(500) NOT NULL,
     message_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (sender_id) REFERENCES account(account_id) ON DELETE CASCADE,
-    FOREIGN KEY (receiver_id) REFERENCES account(account_id) ON DELETE CASCADE
+    status VARCHAR(50) NOT NULL,
+    version INT NOT NULL DEFAULT 0,
+    FOREIGN KEY (sender_id) REFERENCES account(account_id),
+    FOREIGN KEY (receiver_id) REFERENCES account(account_id)
 );
+
+CREATE INDEX idx_sender_id ON message(sender_id);
+CREATE INDEX idx_receiver_id ON message(receiver_id);
+CREATE INDEX idx_message_date ON message(message_date);
